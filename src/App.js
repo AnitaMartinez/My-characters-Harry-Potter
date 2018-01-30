ca6dd15442412abdd429f30b3b1c92507ec6f1ce
“Ana” <amartinezaguilar6@gmail.com>
2018-01-30 13:55:15 +0100

    Change structure components to put states in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 import React, { Component } from 'react';
-import ListCharacters from './components/ListCharacters.js';
+import Character from "./components/Character.js"
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      infoCharacters: []
+    }
+
+    this.fetchInfoCharacters = this.fetchInfoCharacters.bind(this);
+  }
+
+  fetchInfoCharacters() {
+    const ENDPOINT = "http://hp-api.herokuapp.com/api/characters";
+    fetch(ENDPOINT)
+    .then((response) => response.json())
+    .then((data) => {
+      this.setState({
+        infoCharacters: data
+      });
+    });
+  }
+
+  paintListCharacters() {
+    return this.state.infoCharacters.map((object, index) => {
+      let listOfCharacters =
+      <Character
+      key = {index}
+      name = {object.name}
+      image = {object.image}
+      altImage = {object.actor}
+      house = {object.house}
+      condition = {object.alive ? "alive" :  "dead" }
+      />
+      return listOfCharacters
+    });
+  };
+
   render() {
     return (
       <div>
@@ -10,12 +46,17 @@ class App extends Component {
         </header>
         <main>
           <ul>
-            <ListCharacters/>
+            {this.paintListCharacters()}
           </ul>
         </main>
       </div>
     );
   }
+
+  componentDidMount() {
+    this.fetchInfoCharacters();
+  }
+
 }
 
 export default App;
