b15b6679822d6b5d41f8587412ba4423b602fa78
“Ana” <amartinezaguilar6@gmail.com>
2018-01-31 17:08:09 +0100

    Reorganize images en their rutes

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import iconDead from '../images/dead.png'
+// import iconDead from './public/images/dead.png'
 
 
 const Character = ({name, image, altImage, house, condition, coreWand}) => {
@@ -11,8 +11,8 @@ const Character = ({name, image, altImage, house, condition, coreWand}) => {
       </div>
       <div className="text-center">
         <h4 className="tittle-character"> {name} </h4>
-        <div className={`${house} center`} ></div>
-        <img src={iconDead} alt="Icon death" className={`${condition} icon-death`}/>
+        <div className={`${house} center icon-house`} ></div>
+        <img src="images/dead.png" alt="Icon death" className={`${condition} icon-death`}/>
         <p>{name}''s magic wand contains: {coreWand}.</p>
       </div>
     </li>
